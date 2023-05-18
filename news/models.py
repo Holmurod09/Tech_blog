1f62c92f8bb3a025e355908c2847a93d66ebd709
Holmurod09 <rajabovholmurod314@gmail.com>
2023-05-18 14:22:27 +0500

    Upgrade

--- news/models.py
+++ news/models.py
@@ -1,8 +1,43 @@
 from django.db import models
 
 class Gadgets(models.Model):
-    images = models.ImageField()
+    images = models.ImageField(upload_to="media/")
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Videos(models.Model):
+    images = models.ImageField(upload_to="media/")
+    faceboook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    google = models.URLField(blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.CharField(max_length=100)
+    who = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Reviews(models.Model):
+    images = models.ImageField(upload_to="media/")
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Contatct(models.Model):
+    full_name = models.CharField(max_length=200)
+    phohe = models.CharField(max_length=20)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 
--- news/views.py
+++ news/views.py
@@ -7,4 +7,7 @@ def home(request):
 def gadgets(request):
     gadgets = Gadgets.objects.all()
     context = {'gadgets':gadgets}
-    return render(request, )
+    return render(request, "gadgates/tech-category-01.html", context)
+
+
+    
