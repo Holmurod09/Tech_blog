41ab62e10a1fc2cb2da07d94714548c6b9373802
Holmurod09 <rajabovholmurod314@gmail.com>
2023-05-18 13:16:48 +0500

    upgrade

--- news/views.py
+++ news/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'tech-index.html')
+
+
+def gadgets(request):
+    gadgets = Gadgets.objects.all()
+    context = {'gadgets':gadgets}
+    return render(request, )
