b0281374b82ef2509ebd2a01885304f0dfb98b91
Siroj <72664153+Siroj910@users.noreply.github.com>
2023-05-22 21:22:01 +0500

    Update settings.py

--- Tech/settings.py
+++ Tech/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] # Yozilmagan avval !!!
+
 MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
